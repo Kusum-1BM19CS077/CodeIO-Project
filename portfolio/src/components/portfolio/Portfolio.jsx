f7b2068bf78d52dc14bf8428acc54d81dce4033d
Kusum-1BM19CS077 <71306967+Kusum-1BM19CS077@users.noreply.github.com>
2021-07-21 12:27:16 +0530

    Add files via upload

---
+++ portfolio/src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
+import "./portfolio.scss";
+import {
+  featuredPortfolio,
+  webPortfolio,
+  dsPortfolio,
+  cloudPortfolio,
+  openCVPortfolio,
+} from "../../data";
+
+export default function Portfolio() {
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
+  const list = [
+    {
+      id: "featured",
+      title: "Overview",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "ds",
+      title: "DS and Algo",
+    },
+    {
+      id: "cloud",
+      title: "Cloud computing",
+    },
+    {
+      id:"openCV",
+      title:"Python and OpenCV",
+    },
+  ];
+
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "ds":
+        setData(dsPortfolio);
+        break;
+      case "cloud":
+        setData(cloudPortfolio);
+        break;
+        case "openCV":
+        setData(openCVPortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+  }, [selected]);
+
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Interests</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+      <div className="container">
+        {data.map((d) => (
+          <div className="item">
+            <img
+              src={d.img}
+              alt=""
+            />
+            <h3>{d.title}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
